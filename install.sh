e2ab4deb5d0bae7a98344d27b6f735ffb12e811f
Jorge Plasencia <jorgeplasenciaaj@gmail.com>
2024-06-19 10:40:26 -0600

    added install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+OS="$(uname -s)"
+
+# Common operations for Linux and macOS
+rm -rf "$XDG_CONFIG_HOME/zsh"
+ln -s "$DOTFILES/zsh" "$XDG_CONFIG_HOME"
+
+rm -rf "$HOME/.zshenv"
+ln -s "$DOTFILES/zsh/.zshenv" "$HOME"
+
+case "$OS" in
+    # Linux)
+    #     mkdir -p "$XDG_DATA_HOME"
+    #     cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME/fonts"
+    #     ;;
+    Darwin)
+        rm -rf "$XDG_CONFIG_HOME/yabai"
+        ln -s "$DOTFILES/yabai" "$XDG_CONFIG_HOME"
+
+        rm -rf "$XDG_CONFIG_HOME/skhd"
+        ln -s "$DOTFILES/skhd" "$XDG_CONFIG_HOME"
+        ;;
+    *)
+        echo "Unsupported OS: $OS"
+        exit 1
+        ;;
+esac
