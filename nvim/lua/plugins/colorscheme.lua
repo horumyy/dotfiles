150e513ff49572a2637e068842cadcf5ccb4d6e5
horumy <hello@horumy.dev>
2024-10-07 19:56:15 -0700

    updated neovim dotfiles

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,8 @@ return {
       vim.g.sonokai_enable_italic = 1
       vim.g.sonokai_style = "andromeda"
       vim.cmd.colorscheme("sonokai")
+      vim.g.sonokai_diagnostic_text_highlight = 1
+      vim.g.sonokai_diagnostic_virtual_text = "colored"
     end,
   },
 }
