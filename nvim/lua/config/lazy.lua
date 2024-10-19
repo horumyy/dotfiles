692a46d40a78180211b3d548f991689560db3cdd
Horumy <jorgeplasenciaaj@gmail.com>
2024-10-18 19:49:29 -0700

    prettier

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
 					neovim = true,
 				},
 			},
-		},
+		},    { import = "lazyvim.plugins.extras.lsp.none-ls" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
