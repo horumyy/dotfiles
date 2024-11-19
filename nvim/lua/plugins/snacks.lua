a7073569e79c0e9e501ff7601ffc2e204edd8683
jorgeatlolasux <jorgeplasenciaaj@gmail.com>
2024-11-18 23:00:12 -0600

    removed useless snacks.nvim shit

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = { enabled = false },
+    notifier = { enabled = false },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = false },
+    words = { enabled = true },
+  },
+}
