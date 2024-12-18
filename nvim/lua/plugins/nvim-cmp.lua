f4544e1bd8e6997f84dfd9bfda6cfe76a07c0d2e
jorgeatlolasux <jorgeplasenciaaj@gmail.com>
2024-12-18 13:34:28 -0700

    removed copilot, added supermaven

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -10,7 +10,13 @@ return {
 
     local luasnip = require("luasnip")
     local cmp = require("cmp")
-
+    if vim.g.ai_cmp then
+      table.insert(opts.sources, 1, {
+        name = "supermaven",
+        group_index = 1,
+        priority = 100,
+      })
+    end
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
